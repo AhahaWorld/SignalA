81feaf2b0ec15187e46c3e730a01da09ac2abd32
aruru-weed <aruzenyukkuri@gmail.com>
2022-06-01 02:48:13 +0900

    fix Feature version type (double -> String)

--- src/main/java/info/ahaha/signala/metasignal/Feature.java
+++ src/main/java/info/ahaha/signala/metasignal/Feature.java
@@ -4,9 +4,9 @@ import java.io.Serializable;
 
 public class Feature implements Serializable {
     public final String name;
-    public final double version;
+    public final String version;
 
-    public Feature(String name, double version) {
+    public Feature(String name, String version) {
         this.name = name;
         this.version = version;
     }
